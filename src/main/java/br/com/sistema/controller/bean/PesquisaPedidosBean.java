a417f7bc7e5bccffd81fe8161b019ca076ed5b68
zucoloto <zucoloto.com@gmail.com>
2015-04-09 23:05:50 -0300

    Sistemas Comerciais Java EE com CDI, JPA e PrimeFaces

---
+++ src/main/java/br/com/sistema/controller/bean/PesquisaPedidosBean.java
@@ -0,0 +1,26 @@
+package br.com.sistema.controller.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaPedidosBean {
+
+	private List<Integer> produtosFiltrados;
+
+	public PesquisaPedidosBean() {
+		produtosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			produtosFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getProdutosFiltrados() {
+		return produtosFiltrados;
+	}
+
+}
